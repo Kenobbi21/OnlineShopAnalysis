ca835f403d27fc581c95859e7696eaf7bacbb312
Kenobbi21 <ruslik.emashev@gmail.com>
2025-05-04 15:28:09 +0500

    prcnt.sql
    amount of customers, total check sum, avg check price by city

--- prcnt.sql
+++ prcnt.sql
@@ -1,4 +1,6 @@
-SELECT CONCAT(YEAR(O.order_purchase_timestamp),"-",MONTH(O.order_purchase_timestamp)) AS date, COUNT(DISTINCT C.customer_unique_id) AS customers, COUNT(order_id) / COUNT(DISTINCT C.customer_unique_id) - 1 AS same_customer_procent
-FROM customers AS C JOIN orders AS O ON C.customer_id = O.customer_id
-GROUP BY date
-ORDER BY LEFT(date, 4), RIGHT(date, 2)
+SELECT customer_city, COUNT(customer_unique_id) AS customers, SUM(payment_value), SUM(payment_value) / COUNT(orders_order_id) AS avgnum
+FROM customers AS C JOIN orders AS O ON C.customer_id = O.order_customer_id JOIN order_payments OP ON O.orders_order_id = OP.order_id
+WHERE YEAR(order_purchase_timestamp) = "2017"
+GROUP BY customer_city
+ORDER BY customers desc
+limit 152
